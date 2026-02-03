1e493c2c9db815e9c5a15dc20be6375fd97fa507
Sourav-Tripathy <lipuntripathy74@gmail.com>
2026-02-03 22:36:11 +0530

    curations page addition

---
+++ public/projects/project.js
@@ -0,0 +1,18 @@
+const projects = [
+    {
+        category: "Browser Extension",
+        title: "ISS Distance Tracker",
+        url: "https://chromewebstore.google.com/detail/iss-distance-tracker/hcleaonfidpimalcjakemloadmejpiff",
+        description: "A web extension that tells you how far is the ISS from you. It notifies you when the ISS is passing over you or is within 800km from you.",
+        footerHtml: 'Available on <a href="https://chromewebstore.google.com/detail/iss-distance-tracker/hcleaonfidpimalcjakemloadmejpiff" target="_blank">Chrome Web Store</a>',
+        highlightColor: "var(--highlight)"
+    },
+    {
+        category: "Knowledge Management",
+        title: "Cognitia",
+        url: "https://cognitia.club",
+        description: "A knowledge garden for internet readings where one can share and track all reading with other people inside it.",
+        footerHtml: '(Currently in dev version) — <a href="https://cognitia.club" target="_blank">cognitia.club</a>',
+        highlightColor: "var(--highlight)"
+    }
+];
