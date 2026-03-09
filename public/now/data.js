e15b1c0151459249effe2b1e1c8030bf09131295
Sourav-Tripathy <lipuntripathy74@gmail.com>
2026-03-09 22:49:24 +0530

    update

--- public/now/data.js
+++ public/now/data.js
@@ -5,6 +5,12 @@ window.nowData = {
             titleLink: "https://www.goodreads.com/book/show/111185.Physics_and_Philosophy",
             author: "Werner Heisenberg",
             authorLink: "https://en.wikipedia.org/wiki/Werner_Heisenberg"
+        },
+        {
+            title: "Love in the Time of Cholera",
+            titleLink: "https://www.goodreads.com/book/show/9712.Love_in_the_Time_of_Cholera",
+            author: "Gabriel García Márquez",
+            authorLink: "https://en.wikipedia.org/wiki/Gabriel_García_Márquez"
         }
     ],
     listening: [
