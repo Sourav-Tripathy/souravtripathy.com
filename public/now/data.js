026ed56b28641de3c90c76cc7b7f46ebab843678
Sourav-Tripathy <lipuntripathy74@gmail.com>
2026-05-25 22:59:55 +0530

    now  update

--- public/now/data.js
+++ public/now/data.js
@@ -1,28 +1,27 @@
 window.nowData = {
-    reading: [
-        {
-            title: "Physics and Philosophy",
-            titleLink: "https://www.goodreads.com/book/show/111185.Physics_and_Philosophy",
-            author: "Werner Heisenberg",
-            authorLink: "https://en.wikipedia.org/wiki/Werner_Heisenberg"
-        },
-        {
-            title: "Gödel, Escher, Bach: An Eternal Golden Braid",
-            titleLink: "https://www.goodreads.com/book/show/24113.G_del_Escher_Bach",
-            author: "Douglas Hofstadter",
-            authorLink: "https://en.wikipedia.org/wiki/Douglas_Hofstadter"
-        },
-        {
-            title: "Love in the Time of Cholera",
-            titleLink: "https://www.goodreads.com/book/show/9712.Love_in_the_Time_of_Cholera",
-            author: "Gabriel García Márquez",
-            authorLink: "https://en.wikipedia.org/wiki/Gabriel_García_Márquez"
-        }
-    ],
-    listening: [
-        {
-            title: "Nocturne No. 2 in E flat Major, Op. 9,2 (Frédéric Chopin)",
-            link: "https://music.youtube.com/search?q=chopin+nocurne+9+opus+2+e+major"
-        }
-    ]
+  reading: [
+    {
+      title: "Gödel, Escher, Bach: An Eternal Golden Braid",
+      titleLink: "https://www.goodreads.com/book/show/24113.G_del_Escher_Bach",
+      author: "Douglas Hofstadter",
+      authorLink: "https://en.wikipedia.org/wiki/Douglas_Hofstadter",
+    },
+    {
+      title: "Love in the Time of Cholera",
+      titleLink:
+        "https://www.goodreads.com/book/show/9712.Love_in_the_Time_of_Cholera",
+      author: "Gabriel García Márquez",
+      authorLink: "https://en.wikipedia.org/wiki/Gabriel_García_Márquez",
+    },
+  ],
+  listening: [
+    {
+      title: "Nocturne No. 2 in E flat Major, Op. 9,2 (Frédéric Chopin)",
+      link: "https://music.youtube.com/search?q=chopin+nocurne+9+opus+2+e+major",
+    },
+    {
+      title: "The 4 Seasons, Op. 8/4, RV 297, 'Winter'",
+      link: "https://music.youtube.com/watch?v=_FeJFs6LJjU",
+    },
+  ],
 };
