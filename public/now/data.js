480ee341ac0e686a07eccb217b2e8fbf9c62ad91
Sourav-Tripathy <lipuntripathy74@gmail.com>
2026-07-09 20:55:58 +0530

    update

--- public/now/data.js
+++ public/now/data.js
@@ -7,12 +7,6 @@ window.nowData = {
       authorLink: "https://patchenbarss.com",
     },
     {
-      title: "South of the Border, West of the Sun",
-      titleLink: "https://www.goodreads.com/book/show/17799.South_of_the_Border_West_of_the_Sun",
-      author: "Haruki Murakami",
-      authorLink: "https://en.wikipedia.org/wiki/Haruki_Murakami",
-    },
-    {
       title: "Gödel, Escher, Bach: An Eternal Golden Braid",
       titleLink: "https://www.goodreads.com/book/show/24113.G_del_Escher_Bach",
       author: "Douglas Hofstadter",
