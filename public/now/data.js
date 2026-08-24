7c5a7984609c05151178fe4d3700c9ce7596b690
Sourav-Tripathy <lipuntripathy74@gmail.com>
2026-08-24 23:45:33 +0530

    update

--- public/now/data.js
+++ public/now/data.js
@@ -1,12 +1,6 @@
 window.nowData = {
   reading: [
     {
-      title: "When we cease to understand the world",
-      titleLink: "https://www.goodreads.com/book/show/62069739-when-we-cease-to-understand-the-world",
-      author: "Benjamín Labatut",
-      authorLink: "https://en.wikipedia.org/wiki/Benjam%C3%ADn_Labatut",
-    },
-    {
       title: "Gödel, Escher, Bach: An Eternal Golden Braid",
       titleLink: "https://www.goodreads.com/book/show/24113.G_del_Escher_Bach",
       author: "Douglas Hofstadter",
