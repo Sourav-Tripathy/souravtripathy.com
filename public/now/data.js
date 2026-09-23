f6719346dfe32deb15e9f3820c0c470299727a75
Sourav-Tripathy <lipuntripathy74@gmail.com>
2026-09-23 15:52:14 +0530

    update

--- public/now/data.js
+++ public/now/data.js
@@ -1,17 +1,13 @@
 window.nowData = {
   reading: [
     {
-      title: "I,Robot",
-      titleLink: "https://www.goodreads.com/en/book/show/41804.I_Robot",
-      author: "Isaac Asimov",
-      authorLink: "https://en.wikipedia.org/wiki/Isaac_Asimov",
+      title: "The Sense of an Ending",
+      titleLink: "https://www.goodreads.com/book/show/10746542-the-sense-of-an-ending",
+      author: "Julian Barnes",
+      authorLink: "https://en.wikipedia.org/wiki/Julian_Barnes",
     }
   ],
   listening: [
-    {
-      title: "Nocturne No. 2 in E flat Major, Op. 9,2 (Frédéric Chopin)",
-      link: "https://music.youtube.com/search?q=chopin+nocurne+9+opus+2+e+major",
-    },
     {
       title: "The 4 Seasons, Op. 8/4, RV 297, 'Winter'",
       link: "https://music.youtube.com/watch?v=_FeJFs6LJjU",
